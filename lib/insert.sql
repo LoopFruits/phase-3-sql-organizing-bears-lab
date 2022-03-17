15f4e2b4dc1cae7b2f8939503d60903894e11b51
LoopFruits <Kensworldc@gmail.com>
2022-03-17 17:08:13 -0400

    completed assignment, felt hella confident

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name , sex ) VALUES ('Mr. Chocolate', 'M');
+INSERT INTO bears (name , sex ) VALUES ('Rowdy', 'M');
+INSERT INTO bears (name , sex ) VALUES ('Tabitha', 'W');
+INSERT INTO bears (name , sex ) VALUES ('Sergeant Brown', 'M');
+INSERT INTO bears (name , sex ) VALUES ('Melissa', 'W');
+INSERT INTO bears (name , sex ) VALUES ('Grinch', 'M');
+INSERT INTO bears (name , sex ) VALUES ('Wendy', 'W');
+INSERT INTO bears (name , sex ) VALUES (NULL, NULL);
